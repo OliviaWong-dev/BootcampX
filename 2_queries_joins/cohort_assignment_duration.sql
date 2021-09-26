518db42c033f5326a82db4b6ef3465186d01b9dd
Olivia Wong <olivia.wy.wong@gmail.com>
2021-09-26 18:50:35 +0000

    added teachers and assistance_request tables and input test data

--- 2_queries_joins/cohort_assignment_duration.sql
+++ 2_queries_joins/cohort_assignment_duration.sql
@@ -3,3 +3,12 @@ FROM assignment_submissions
 JOIN students ON student_id = students.id 
 JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name='FEB12';
+
+
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+HAVING count(assignment_id) < 100
+GROUP BY students.name;
