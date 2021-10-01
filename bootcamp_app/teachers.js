fa4290fb008a2d98f140f94925ff9dd2efb95de7
Olivia Wong <olivia.wy.wong@gmail.com>
2021-10-01 19:02:54 +0000

    parameterize students and teachers queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,8 @@
 const { Pool } = require('pg');
 
+const cohortName = process.argv[2];
+const values = [cohortName];
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,16 +10,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`)
